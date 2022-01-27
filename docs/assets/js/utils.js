987e6ff647f8946f21eaf1e1e740a5175c8f06f2
Mara <mara-li@outlook.fr>
2022-01-27 19:47:45 +0100

    fix(wikilinks): Use ez-links with my fork
    Waiting to PR

--- docs/assets/js/utils.js
+++ docs/assets/js/utils.js
@@ -1,29 +1,18 @@
-function heading(str) {
-    var title_regex = /\[{2}(.*)\|(.*)/gi
-    str_list = str.split(']]')
-    for (var i = 0; i < str_list.length; i++) {
-        truc = str_list[i].match(title_regex);
-        if (truc) {
-            title = truc.join().split('|')[1]
-            title = '[[' + title
-            str = str.replace(str_list[i].match(title_regex), title)
-        }
+
+
+
+var p_search = /\.\.%5C/gi
+var ht = document.querySelectorAll('a');
+for (var i = 0; i < ht.length; i++) {
+    var link = ht[i].href.match(p_search);
+    if (link) {
+        const newItem = document.createElement('div');
+        newItem.innerHTML = ht[i].innerHTML;
+        newItem.classList.add('not_found');
+        ht[i].parentNode.replaceChild(newItem, ht[i]);
     }
-    return str
 }
 
-var p_search = /!?\[{2}(.*)\]{2}/gi
-var ht = document.querySelectorAll('article.md-content__inner.md-typeset > *:not(.highlight)');
-for (var i = 0; i < ht.length; i++) {
-    const found_p = ht[i].innerHTML.match(p_search);
-    if (found_p) {
-        founded = heading(found_p[0])
-        var not_found = founded.replace(/]]/gi, '</div>');
-        not_found = not_found.replace(/\[\[/gi, '<div class="not_found">')
-        not_found = not_found.replace('!', '')
-        ht[i].innerHTML = ht[i].innerHTML.replace(found_p, not_found);
-    }
-}
 
 var p_img = /\.+\\/gi
 var img = document.querySelectorAll('img');
