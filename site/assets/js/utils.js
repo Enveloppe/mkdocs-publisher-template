3509349cc7030a76c821f7a025a2ba0a5fc952ac
Mara <mara-li@outlook.fr>
2022-04-08 19:06:17 +0200

    Actions workflow

---
+++ site/assets/js/utils.js
@@ -0,0 +1,61 @@
+function UrlExists(url) {
+    ref = url.href
+    var http = new XMLHttpRequest();
+    http.open('GET', ref, true);
+    http.onload=function(e) {
+        if (http.status == '404') {
+            const newItem = document.createElement('div');
+            newItem.innerHTML = url.innerHTML;
+            newItem.classList.add('not_found');
+            url.parentNode.replaceChild(newItem, url);
+        }
+        else {
+            return true;
+        }
+    }
+    http.send()
+}
+
+
+var p_search = /\.{2}\//gi
+not_found = []
+var ht = document.querySelectorAll('a');
+for (var i = 0; i < ht.length; i++) {
+    var link = UrlExists(ht[i]);
+}
+
+var p_img = /\.+\\/gi
+var img = document.querySelectorAll('img');
+for (var i = 0; i < img.length; i++) {
+    (img[i].attributes.src.nodeValue)
+    if (img[i].alt.match(/\|?\d+$/)) {
+        img[i].width = img[i].alt.match(/\|?\d+$/)[0].replace('|', '')
+    }
+}
+
+
+
+var ht = document.querySelectorAll('article.md-content__inner.md-typeset > *:not(.highlight)');
+var scr=/\^(.*)/gi;
+for (var i = 0; i <ht.length;i++){
+    const fp=ht[i].innerHTML.match(scr)
+	if (fp) {
+        ht[i].innerHTML=ht[i].innerHTML.replace(fp, '')
+    }
+}
+document.innerHTML = ht;
+
+var cite = document.querySelectorAll('.citation');
+if (cite) {
+    for (var i = 0; i < cite.length; i++) {
+        var img = cite[i].innerHTML.match(/!?(\[{2}|\[).*(\]{2}|\))/gi)
+        if (img) {
+            for (var j = 0; j < img.length; j++) {
+                cite[i].innerHTML = cite[i].innerHTML.replace(img[j], '')
+            }
+            if (cite[i].innerText.trim().length < 2) {
+                cite[i].style.display='none';
+            }
+            }
+        }
+    }
