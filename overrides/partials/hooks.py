4a8e7cb49a5cf77e93b92757e1814af1986dee5f
Mara-Li <mara-li@outlook.fr>
2022-06-17 22:26:18 +0000

    🔄 Synced local 'overrides/' with remote 'overrides/'

--- overrides/partials/hooks.py
+++ overrides/partials/hooks.py
@@ -25,13 +25,7 @@ def time_to_iso(time):
         return time
 
 def on_env(env, config, files, **kwargs):
-    print('locale to set:', config['theme']['language'])
     print('get locale', locale.getlocale())
-    locale_toset = config['theme']['language']
-    if (locale_toset != locale_toset + '_' + locale_toset.upper()):
-        locale_toset = locale_toset + '_' + locale_toset.upper()
-    print(locale_toset)
-    locale.setlocale(locale.LC_TIME, locale_toset)
     env.filters['convert_time'] = time_time
     env.filters['iso_time'] = time_to_iso
     return env
