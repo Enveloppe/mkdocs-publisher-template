b3544e60a23cf974d690f15d787ae7e58629fc25
Mara-Li <mara-li@outlook.fr>
2022-06-17 21:01:14 +0000

    🔄 Synced local 'overrides/' with remote 'overrides/'

---
+++ overrides/partials/hooks.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from dateutil import parser
+import locale
+
+
+
+def time_time(time):
+    time=time.replace('-', '/')
+    time = parser.parse(time).isoformat()
+    try:
+        time = datetime.fromisoformat(time)
+        return datetime.strftime(time,'%d %B %Y')
+    except AttributeError:
+        return datetime.strftime(str(time),'%d %B %Y')
+    except ValueError:
+        print('value error!')
+        return time
+
+def time_to_iso(time):
+    time=time.replace('-', '/')
+    
+    try:
+        return parser.parse(time).isoformat()
+    except AttributeError:
+        return time
+
+def on_env(env, config, files, **kwargs):
+    locale.setlocale(locale.LC_TIME,config['theme']['language'])
+    env.filters['convert_time'] = time_time
+    env.filters['iso_time'] = time_to_iso
+    return env
