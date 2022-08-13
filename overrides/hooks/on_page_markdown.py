1bc065cc0e0d6592cc34fdf64b4aea52ae38da8f
Mara-Li <mara-li@outlook.fr>
2022-08-13 09:40:26 +0000

    🔄 Synced local 'overrides/' with remote 'overrides/'

--- overrides/hooks/on_page_markdown.py
+++ overrides/hooks/on_page_markdown.py
@@ -1,5 +1,21 @@
 import re
 
+def update_heading(markdown):
+    file_content = markdown.split('\n')
+    markdown = ''
+    code = False
+    for line in file_content:
+        if not code:
+            if line.startswith('```'):
+                code = True
+            elif line.startswith('#') and line.count('#')<=5:
+                heading_number = line.count('#') + 1
+                line = '#' * heading_number + ' ' + line.replace('#', '')
+        elif line.startswith('```') and code:
+            code = True
+        markdown += line + '\n'
+    return markdown
+
 def strip_comments(markdown):
     file_content = markdown.split('\n')
     markdown = ''
@@ -19,6 +35,7 @@ def fix_tags(metadata):
 
 def on_page_markdown(markdown, files, page, **kwargs):
     markdown = strip_comments(markdown)
+    markdown = update_heading(markdown)
     metadata = fix_tags(page.meta)
     page.meta = metadata
     return markdown
