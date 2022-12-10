d76be8cb19d7eabe73dbf5ae4eca9e66d3cb8463
Lisandra-dev <null>
2022-12-10 02:41:11 +0000

    🔄 Synced local 'overrides/' with remote 'overrides/'

--- overrides/hooks/on_env.py
+++ overrides/hooks/on_env.py
@@ -7,10 +7,16 @@ import obsidiantools.api as otools
 import os
 import shutil
 from pyvis.network import Network
+import logging
+
 
 
 def obsidian_graph():
-    print('generate obsidian graph')
+    '''
+    Generates a graph of the Obsidian vault.
+    '''
+    log = logging.getLogger('mkdocs.plugins.' + __name__)
+    log.info("[OBSIDIAN GRAPH] Generating graph...")
     vault = otools.Vault(os.getcwd()).connect().gather()
     graph = vault.graph
     net = Network(
@@ -24,19 +30,34 @@ def obsidian_graph():
     except OSError:
         pass
     shutil.rmtree(Path(os.getcwd(), 'lib'))
-    print('done')
+    log.info("[OBSIDIAN GRAPH] Graph generated!")
     return ''
 
 
 obsidian_graph()
 
 
-def logging(text):
+def log(text):
+    '''
+    Prints text to the console, in case you need to debug something.
+    Using mainly in the template files.
+    Parameters:
+        text (str): The text to print.
+    Returns:
+        str: An empty string.
+    '''
     print(text)
     return ''
 
 
 def time_time(time):
+    '''
+    Converts a time string to a human-readable format.
+    Parameters:
+        time (any): The time string to convert.
+    Returns:
+        str|datetime:  The converted time.
+    '''
     time = time.replace('-', '/')
     time = parser.parse(time).isoformat()
     try:
@@ -50,16 +71,41 @@ def time_time(time):
 
 
 def to_local_time(time, locale):
+    """Convert to local time.
+
+    Args:
+        time (any): the time to convert
+        locale (any): the locale to use
+
+    Returns:
+        str: the converted time
+    """
     date = time.replace('-', '/')
     date = parser.parse(date)
     return format_date(date, locale=locale)
 
 
 def time_todatetime(time):
+    """convert time to datetime
+
+    Args:
+        time (any): time to convert
+
+    Returns:
+        datetime: the converted time
+    """
     return parser.parse(time)
 
 
 def time_to_iso(time):
+    """Convert time to ISO format.
+
+    Args:
+        time (any): Time to convert
+
+    Returns:
+        any|str: convert time or the original time if error
+    """
     time = time.replace('-', '/')
 
     try:
@@ -69,10 +115,26 @@ def time_to_iso(time):
 
 
 def page_exists(page):
+    """Check if a page exists.
+
+    Args:
+        page (any): The page to check
+
+    Returns:
+        bool: true if exists
+    """
     return Path(page).exists()
 
 
 def url_decode(url):
+    """decode an url in a template
+
+    Args:
+        url (any): THE URL 
+
+    Returns:
+        str : the decoded url
+    """
     return urllib.parse.unquote(url)
 
 
@@ -82,6 +144,6 @@ def on_env(env, config, files, **kwargs):
     env.filters['time_todatetime'] = time_todatetime
     env.filters['page_exists'] = page_exists
     env.filters['url_decode'] = url_decode
-    env.filters['logging'] = logging
+    env.filters['log'] = log
     env.filters['to_local_time'] = to_local_time
     return env
